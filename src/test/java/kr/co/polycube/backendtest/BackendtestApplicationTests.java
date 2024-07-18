46d2762e158fe0bc636eb3b15b687f38f28d01b3
getinthere <getinthere@naver.com>
2024-07-18 10:05:00 +0900

    [feat] project init

---
+++ src/test/java/kr/co/polycube/backendtest/BackendtestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.polycube.backendtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
